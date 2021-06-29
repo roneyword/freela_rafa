f28d933915257d0a893e8960ce155b28ff740672
roneyword <roneyword@hotmail.com>
2021-06-28 21:21:28 -0300

    criação da pagina engine

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
-import scrollSmooth from "./modules/smooth_scroll.js";
 import headerNav from "./modules/headerNav.js";
 let headerHeight = document.querySelector("header").clientHeight;
 
 headerNav();
-scrollSmooth(headerHeight);
 
 window.addEventListener("resize", () => {
   headerHeight = document.querySelector("header").clientHeight;
